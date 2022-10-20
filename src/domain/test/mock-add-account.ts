2f9a1c9acf9ce48d34410e017aad32480498986a
Maskalenkas <maskalenkasdoc@gmail.com>
2022-10-20 00:04:45 -0300

    feat: mock function add to usecase addAccount

---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker'
+
+import { AddAccount } from '@/domain/usecases/add-account'
+
+export const mockAddAccountParams: AddAccount.Params = {
+  email: faker.internet.email(),
+  name: faker.name.firstName(),
+  password: faker.internet.password()
+}
+
+export const mockAddAccountResult: AddAccount.Result = true
