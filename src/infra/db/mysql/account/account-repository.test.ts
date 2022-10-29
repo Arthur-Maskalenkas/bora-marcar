54fbc6da72564c02a10fc2ba92db8b34f423ea35
Maskalenkas <maskalenkasdoc@gmail.com>
2022-10-29 16:24:52 -0300

    feat: Docker adicionado

---
+++ src/infra/db/mysql/account/account-repository.test.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client'
+
+import { AccountRepository } from './account-repository'
+
+import { mockAddAccountParams } from '@/domain/test/mock-add-account'
+
+type SutTypes = {
+  sut: AccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AccountRepository()
+  return {
+    sut
+  }
+}
+
+describe('AccountRepository', () => {
+  const prisma = new PrismaClient()
+
+  beforeAll(async () => {
+    await prisma.$connect()
+  })
+
+  afterAll(async () => {
+    const deleteAccounts = prisma.account.deleteMany()
+
+    await prisma.$transaction([
+      deleteAccounts
+    ])
+
+    await prisma.$disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const mockParams = mockAddAccountParams()
+
+    await sut.add(mockParams)
+
+    const account = await prisma.account.findUnique({
+      where: {
+        email: mockParams.email
+      }
+    })
+
+    expect(account).toBeTruthy()
+  })
+})
