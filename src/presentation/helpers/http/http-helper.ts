0ed287ba52b6b30297559515c65a74115bdae71b
Maskalenkas <maskalenkasdoc@gmail.com>
2022-10-30 02:49:24 -0300

    🔥 feat(presentation): add 'forbidden' helper on http-helper

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -5,3 +5,11 @@ export const noContent = (): HttpResponse => ({
   statusCode: 204,
   body: null
 })
+
+// O servidor recebeu a requisição, entendeu, porém é insuficiente para para garantir o acesso
+export const forbidden = (error: Error): HttpResponse => (
+  {
+    statusCode: 403,
+    body: error
+  }
+)
