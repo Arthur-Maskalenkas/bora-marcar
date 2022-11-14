378904c11604a550aa23897b67ac125041d700dc
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2022-11-13 23:55:08 -0300

    first

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,4 +1,4 @@
-import { makeDbAddAccount, makeDbAuthentication } from '@/main/factories/usecases'
+import { makeDbAddAccount, makeDbAuthentication } from '../useCases'
 import { Controller } from '@/presentation/protocols'
 import { makeSignUpValidation } from '@/main/factories/controllers'
 import { SignUpController } from '@/presentation/controllers'
