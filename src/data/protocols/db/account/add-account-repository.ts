c49f3889059572a25d659892563df92b9d52a166
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2022-11-14 01:05:51 -0300

    dsadas

--- src/data/protocols/db/account/add-account-repository.ts
+++ src/data/protocols/db/account/add-account-repository.ts
@@ -4,6 +4,10 @@ export interface AddAccountRepository {
   add: (accountData: AddAccountRepository.Params) => AddAccountRepository.Result
 }
 
+for (var i = 1; i != 10; i += 2) {
+  console.log('olas')
+}
+
 export namespace AddAccountRepository {
   export type Params = AddAccount.Params
   export type Result = Promise<AddAccount.Result>
